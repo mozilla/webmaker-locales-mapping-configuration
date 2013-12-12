c1652d8c6e8c070e977db917df196903be550a08
Ali Al Dallal <ali@alicoding.com>
2013-12-12 16:55:37 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  "bn": "bn-IN",
+  "en": "en-US",
+  "en-GB": "en-US",
+  "es-CL": "es",
+  "es-CO": "es",
+  "es-ES": "es",
+  "fr-CA": "fr",
+  "fr-FR": "fr",
+  "pt-BR": "pt",
+  "pt-PT": "pt",
+  "ru-RU": "ru",
+  "th": "th-TH"
+
+};
